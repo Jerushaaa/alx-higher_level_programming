fb22f7419f4f11fbaa757b8ce5a82a852272d15b
Eyerusalem <jerry.gigar@gmail.com>
2022-08-23 12:56:19 -0700

    vi 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def islower(c): True
-    if (ord(c) >= 97 and ord(c) <= 122):
-        else:
-            False
+if (ord(c) >= 97 and ord(c) <= 122):
+    def islower(c): True
+else:
+    False
