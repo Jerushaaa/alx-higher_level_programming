c67274ca41871bb1dfdc4982dc56a7cd508bb01f
Eyerusalem <jerry.gigar@gmail.com>
2022-12-15 11:09:08 -0800

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Catch me
-curl "0.0.0.0:5000/catch_me" -sLX PUT -H "You got me!" -d "user_id=98"
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sLX PUT -d "user_id=98" 0.0.0.0:5000/catch_me
